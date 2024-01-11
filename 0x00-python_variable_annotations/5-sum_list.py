705d4754dcdc6b053b851b88f717a8950e3af523
Mutisomary <marymutso2016@gmail.com>
2024-01-11 06:45:24 -0800

    sum of list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of the input lists"""
+    return sum(input_list)
