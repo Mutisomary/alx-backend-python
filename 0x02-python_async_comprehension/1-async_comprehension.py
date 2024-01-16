aacdd9c2db112cbf1b62afacaa6666f7f286bbcb
Mutisomary <marymutso2016@gmail.com>
2024-01-15 22:03:36 -0800

    list comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Async comprehensions"""
+    random_number = [i async for i in async_generator()]
+    return random_number
